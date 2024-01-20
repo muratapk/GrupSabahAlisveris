6da83676aeb2eec15da8d754b9905f9d48c5bd77
muratapk <muratapk@hotmail.com>
2024-01-20 14:01:48 +0300

    datatables yapımı

---
+++ GrupSabahAlisveris/Controllers/AdminsController.cs
@@ -0,0 +1,36 @@
+﻿using GrupSabahAlisveris.Data;
+using GrupSabahAlisveris.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GrupSabahAlisveris.Controllers
+{
+    public class AdminsController : Controller
+    {
+
+        private readonly ApplicationDbContext _context;
+
+        public AdminsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+
+            var sorgu = _context.Admins.ToList();
+            return View(sorgu);
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Admin gelen)
+        {
+            _context.Admins.Add(gelen);
+            _context.SaveChanges();
+            TempData["Success"] = "İşlem Başarılı";
+            return RedirectToAction("Index");
+        }
+    }
+}
