cdcdefc6991c17d3e8f65c55dd57894be58a609b
muratapk <muratapk@hotmail.com>
2024-01-28 10:59:37 +0300

    Kategory ViewComponent oluşturul

---
+++ GrupSabahAlisveris/ViewComponents/KategoriMenuList.cs
@@ -0,0 +1,20 @@
+﻿using GrupSabahAlisveris.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GrupSabahAlisveris.ViewComponents
+{
+    public class KategoriMenuList:ViewComponent
+    {
+        private readonly ApplicationDbContext _context;
+
+        public KategoriMenuList(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public IViewComponentResult Invoke()
+        {
+            var sorgu = _context.Categories.ToList();
+            return View(sorgu);
+        }
+    }
+}
