968d538a3f0a4893a283702274c42da2119f2ae0
muratapk <muratapk@hotmail.com>
2024-02-10 11:37:50 +0300

    login İşlemleri yapıldı

--- GrupSabahAlisveris/Controllers/LoginController.cs
+++ GrupSabahAlisveris/Controllers/LoginController.cs
@@ -1,12 +1,35 @@
-﻿using Microsoft.AspNetCore.Mvc;
-
+﻿using GrupSabahAlisveris.Data;
+using Microsoft.AspNetCore.Mvc;
+using GrupSabahAlisveris.Models;
 namespace GrupSabahAlisveris.Controllers
 {
     public class LoginController : Controller
     {
+        private readonly ApplicationDbContext _context;
+
+        public LoginController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        //public LoginController(ApplicationDbContext context)
+        //{
+        //    _context=context;
+        //}
         public IActionResult Index()
         {
             return View();
+        }
+        [HttpPost]
+        public IActionResult EnterLogin(Admin gelen)
+        { //Where 
+            var sorgu = _context.Admins.Where(x => x.AdminName == gelen.AdminName && x.AdminPassword == gelen.AdminPassword).FirstOrDefault();
+            if(sorgu != null) {
+                TempData["Success"] = "İşlem Başarılı";
+                return RedirectToAction("Index", "Categories");
+            }
+            TempData["Error"] = "Kullanıcı ve Şifre Hatalı";
+            return RedirectToAction("Index","Login");
         }
     }
 }
