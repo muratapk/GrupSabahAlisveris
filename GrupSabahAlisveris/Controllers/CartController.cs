cfb75bf22f26ffcbca4ec811d3f859e19f69da3c
muratapk <muratapk@hotmail.com>
2024-02-17 10:29:32 +0300

    Sepete Eklem yapıldı

--- GrupSabahAlisveris/Controllers/CartController.cs
+++ GrupSabahAlisveris/Controllers/CartController.cs
@@ -31,7 +31,20 @@ namespace GrupSabahAlisveris.Controllers
         public async Task<IActionResult>Add(int id)
         {
             Product product = await _context.Products.FindAsync(id);
+            List<CartItem> cart = HttpContext.Session.GetJson<List<CartItem>>("Cart") ?? new List<CartItem>();
+            CartItem cartItem=cart.FirstOrDefault(c=>c.ProductId == id);
+            if(cartItem==null)
+            {
+                cart.Add(new CartItem(product));
+            }
+            else
+            {
+                cartItem.Quantity += 1;
 
+            }
+            HttpContent.Session.SetJson("Cart", cart);
+            TempData["Success"] = "Sepete Eklendi";
+            return RedirectToAction(Request.Headers["Referer"].ToString());
             return View();
         }
     }
